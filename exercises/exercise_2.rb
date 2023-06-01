021eab949bf674249a1866b77871f46b23464777
hoomili <hasadian@hotmail.com>
2023-05-31 18:17:33 -0700

    finished exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,12 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
-puts "Exercise 2"
+@store1 = Stores.find_by(id: 1)
+@store2 = Stores.find_by(id: 2)
+p @store1
+@store1.name = "Downtown"
+p @store1
+
 puts "----------"
 
 # Your code goes here ...
