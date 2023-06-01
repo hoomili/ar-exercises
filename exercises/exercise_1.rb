f145d7e1b7f0b4d6517326f06ec1ee045b3237a5
hoomili <hasadian@hotmail.com>
2023-05-31 18:17:15 -0700

    finished exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,14 @@
 require_relative '../setup'
+require 'active_record'
+class Stores < ActiveRecord::Base
+  
+end
 
-puts "Exercise 1"
+
+burnaby = Stores.create(name: "Burnaby",annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Stores.create(name: "Richmond",annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Stores.create(name: "Gastown",annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+puts "Number of stores created: #{Stores.count}"
 puts "----------"
 
 # Your code goes below here ...
